0e4a887cb0ee10cf6f5adf9a192656ed77c101f3
Abdul Abbou <53510613+aabb1998@users.noreply.github.com>
2021-08-19 23:59:50 +1000

    Added firebase dependencies and simple registration and login

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React, { useEffect, useState } from "react";
+import { Text, StyleSheet } from "react-native";
+import { HomeScreen } from "./Index";
+import { LoginScreen } from "./Index";
+import { RegistrationScreen } from "./Index";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// import { decode, encode } from "base-64";
+// if (!global.btoa) {
+// 	global.btoa = encode;
+// }
+// if (!global.atob) {
+// 	global.atob = decode;
+// }
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	const [loading, setLoading] = useState(true);
+	const [user, setUser] = useState(null);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	return (
+		<NavigationContainer>
+			<Text>Hello</Text>
+			<Stack.Navigator>
+				{user ? (
+					<Stack.Screen name="Home">
+						{(props) => <HomeScreen {...props} extraData={user} />}
+					</Stack.Screen>
+				) : (
+					<>
+						<Stack.Screen name="Login" component={LoginScreen} />
+						<Stack.Screen
+							name="Registration"
+							component={RegistrationScreen}
+						/>
+					</>
+				)}
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
---
+++ Index.js
@@ -0,0 +1,5 @@
+export { default as LoginScreen } from "./src/Screens/LoginScreen/LoginScreen";
+
+export { default as HomeScreen } from "./src/Screens/HomeScreen/HomeScreen";
+
+export { default as RegistrationScreen } from "./src/Screens/RegistrationScreen/RegistrationScreen";
