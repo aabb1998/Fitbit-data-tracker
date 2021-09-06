dec6925332097e22515477ba9bb2cd10392e4106
SOMESH <someshshine@live.com>
2021-09-06 13:36:50 +1000

    Created basic survey card(still needs work)

--- App.js
+++ App.js
@@ -6,6 +6,45 @@ import { LoginScreen } from "./Index";
 import { RegistrationScreen } from "./Index";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+// You can import from local files
+import Survey from './src/Screens/SurveyScreen/SurveyScreen';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        Survey
+      </Text>
+      <Card>
+        <Survey1/>
+      </Card>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
 
 // import { decode, encode } from "base-64";
 // if (!global.btoa) {
