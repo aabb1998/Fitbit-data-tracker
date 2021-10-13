14be3c74a3da8b3160f24cade9f0143b8f83f6c0
Matthew Veldhuizen <37282829+matthew-veldhuizen@users.noreply.github.com>
2021-10-13 17:36:05 +1100

    Database functions added Create and read in progress

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -17,22 +17,77 @@ if (!firebase.apps.length) {
 
 const db = getFirestore(app);
 
-async function getUsers(data, db) {
-    collection("users").doc(uid);
+/*----------------README-----------------------------------
+
+READ type functions can be used to retrieve ID or key values of
+a record in the database before updating or adding a new record
+
+Note that CREATE and UPDATE functions operate in the same way using .set
+If a record already exists with .doc(ID), it will override the
+record instead of creating a new record.
+
+When passing 'data' into a database function, ensure it is created in the correct way
+
+const data = {
+					field1: field1value,
+					field2: field2value,
+					field3: field3value,
+					.,
+					.,
+					fieldn: fieldnvalue,
+				};
+
+------------------END README--------------------------------*/
+
+
+
+
+
+/*-------- START USERS CRUD -------------------*/
+
+//CREATE or UPDATE
+await function createAndUpdateUser(data) {
+    db.collection("users").doc(uid).set({id:data.uid, email:data.email, fullName:data.fullName,})
+}
+
+//READ
+await function getUserDataByID(data) {
+    db.collection("users").doc(data.uid);
+    return sleepDataList;
+}
+
+await function getUsers(data) {
+    db.collection("users").doc(data.uid);
     return userList;
 }
 
-async function createUser(data, db) {
-    firebase.firestore().collection("users").doc(uid).set({id:data.uid, email:data.email, fullName:data.fullName,})
-}
-
-async function createSleepRecord(data, db) {
-    firebase.firestore().collection("sleepData").doc().set({})
-}
-
-async function getSleepRecord(data, db) {
-    collection(sleepData).doc(uid);
+//DELETE
+await function getSleepDataByID(data) {
+    db.collection("sleepData").doc(data.uid);
     return sleepDataList;
 }
+
+/*-------- END USER CRUD ----------*/
+
+/*--------- START SURVEY RESULTS CRUD -----------*/
+
+//CREATE
+await function createSleepRecord(data, db) {
+    db.collection("sleepData").doc(ID).set({})
+}
+
+//READ
+await function getSleepDataByID(data) {
+    db.collection("sleepData").doc(data.uid);
+    return sleepDataList;
+}
+
+//DELETE
+await function getSleepDataByID(data) {
+    db.collection("sleepData").doc(data.uid);
+    return sleepDataList;
+}
+
+/*-------- END SURVEY RESULTS CRUD -----------*/
 
 export { firebase };
