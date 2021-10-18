03f13242f4869ec76c8ebab35f55b2ad308570c3
Matthew Veldhuizen <37282829+matthew-veldhuizen@users.noreply.github.com>
2021-10-18 13:36:28 +1100

    Added daily and weekly survey CRUD
    Added CRUD functions for daily and weekly survey results

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -48,17 +48,17 @@ const data = {
 /*-------- BEGIN USERS CRUD -------------------*/
 
 //CREATE
-async function createUser(data) {
+export async function createUser(data) {
     app.collection("users").doc(data.uid).set({id:data.uid, email:data.email, fullName:data.fullName,})
     console.log('User Record created');
 }
 
 //READ
-async function getAllUsers() { //returns all user records from the database
+export async function getAllUsers() { //returns all user records from the database
     return app.collection("users").get();
 }
 
-async function getUserByID(data) { //returns only the user document with the matching uid
+export async function getUserByID(data) { //returns only the user document with the matching uid
     const record =  app.collection("users").doc(data.uid).get();
     if (!record.exists) {
         console.log('No document with that uid');
@@ -68,7 +68,7 @@ async function getUserByID(data) { //returns only the user document with the mat
     }
 }
 
-async function getUserByField(data) { //pass in the name of the field you want to search and the value to check for
+export async function getUserByField(data) { //pass in the name of the field you want to search and the value to check for
     const record = app.collection("users").where(data.fieldName, '==', data.fieldValue).get();
     if (!record.exists) {
         console.log('No document matches that value');
@@ -79,18 +79,18 @@ async function getUserByField(data) { //pass in the name of the field you want t
 }
 
 //UPDATE
-async function updateUserEmail(data) { //Requires the uid to update the record
+export async function updateUserEmail(data) { //Requires the uid to update the record
     app.collection("users").doc(data.uid).update({ email:data.email });
     console.log('User Email updated');
 }
 
-async function updateUserFullName(data) { //Requires the uid to update the record
+export async function updateUserFullName(data) { //Requires the uid to update the record
     app.collection("users").doc(data.uid).update({ fullName:data.fullName });
     console.log('User Full Name updated');
 }
 
 //DELETE
-async function deleteUserRecord(data) { //deletes entire record, it is possible to delete individual fields but not needed
+export async function deleteUserRecord(data) { //deletes entire record, it is possible to delete individual fields but not needed
     app.collection("users").doc(data.uid).delete();
     console.log('User Record deleted');
 }
@@ -102,7 +102,7 @@ async function deleteUserRecord(data) { //deletes entire record, it is possible 
 /*--------- BEGIN PVT RESULTS CRUD -----------*/
 
 //CREATE
-async function createPVTRecord(data) {
+export async function createPVTRecord(data) {
     const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const length = 20;
     let randomStr = "";
@@ -116,11 +116,11 @@ async function createPVTRecord(data) {
 }
 
 //READ
-async function getAllResults() { //returns all PVT Result records from the database
+export async function getAllPVTResults() { //returns all PVT Result records from the database
     return app.collection("pvtResults").get();
 }
 
-async function getPVTResultByUserID(data) { //returns only the PVT Result document with the matching document ID
+export async function getPVTResultByDocumentID(data) { //returns only the PVT Result document with the matching document ID
     const record =  app.collection("pvtResults").doc(data.docID).get();
     if (!record.exists) {
         console.log('No document with that ID');
@@ -130,7 +130,7 @@ async function getPVTResultByUserID(data) { //returns only the PVT Result docume
     }
 }
 
-async function getResultByField(data) { //pass in the name of the field you want to search and the value to check for
+export async function getPVTResultByField(data) { //pass in the name of the field you want to search and the value to check for
     const record = app.collection("pvtResults").where(data.fieldName, '==', data.fieldValue).get();
     if (!record.exists) {
         console.log('No document matches that value');
@@ -141,23 +141,147 @@ async function getResultByField(data) { //pass in the name of the field you want
 }
 
 //UPDATE
-async function updatePVTTestScore(data) { //Requires the Document ID to update the record
+export async function updatePVTTestScore(data) { //Requires the Document ID to update the record
     app.collection("pvtResults").doc(data.docID).update({ score:data.score });
     console.log('PVT Test Score updated');
 }
 
-async function updatePVTTestDateTime(data) { //Requires the Document ID to update the record
+export async function updatePVTTestDateTime(data) { //Requires the Document ID to update the record
     app.collection("pvtResults").doc(data.docID).update({ testDateTime:data.testDateTime });
     console.log('PVT Test DateTime updated');
 }
 
 //DELETE
-async function deletePVTResultRecord(data) { //deletes entire record, it is possible to delete individual fields but not needed
+export async function deletePVTResultRecord(data) { //deletes entire record, it is possible to delete individual fields but not needed
     app.collection("pvtResults").doc(data.docID).delete();
     console.log('PVT Result Record deleted');
 }
 
 /*-------- END PVT RESULTS CRUD -----------*/
+
+
+
+/*--------- BEGIN DAILY SURVEY RESULTS CRUD -----------*/
+
+//CREATE
+export async function createDailySurveyRecord(data) {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const length = 20;
+    let randomStr = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomNum = Math.floor(Math.random() * characters.length);
+        randomStr += characters[randomNum];
+    }
+    app.collection("dailySurvey").doc(randomStr).set({score:data.score, surveyDateTime:data.testDateTime, surveyType:data.surveyType, uid:data.uid,})
+    console.log('Daily Survey Result Record created');
+}
+
+//READ
+export async function getAllDailySurveyResults() { //returns all Daily Survey Result records from the database
+    return app.collection("dailySurvey").get();
+}
+
+export async function getDailySurveyResultByDocumentID(data) { //returns only the Daily Survey Result document with the matching document ID
+    const record =  app.collection("dailySurvey").doc(data.docID).get();
+    if (!record.exists) {
+        console.log('No document with that ID');
+        return;
+    } else {
+        return record.data;
+    }
+}
+
+export async function getDailySurveyResultByField(data) { //pass in the name of the field you want to search and the value to check for
+    const record = app.collection("dailySurvey").where(data.fieldName, '==', data.fieldValue).get();
+    if (!record.exists) {
+        console.log('No document matches that value');
+        return;
+    } else {
+        return record.data;
+    }
+}
+
+//UPDATE
+export async function updateDailySurveyScore(data) { //Requires the Document ID to update the record
+    app.collection("dailySurvey").doc(data.docID).update({ score:data.score });
+    console.log('Daily Survey Score updated');
+}
+
+export async function updateDailySurveyDateTime(data) { //Requires the Document ID to update the record
+    app.collection("dailySurvey").doc(data.docID).update({ surveyDateTime:data.testDateTime });
+    console.log('Daily Survey DateTime updated');
+}
+
+//DELETE
+export async function deleteDailySurveyRecord(data) { //deletes entire record, it is possible to delete individual fields but not needed
+    app.collection("dailySurvey").doc(data.docID).delete();
+    console.log('Daily Survey Record deleted');
+}
+
+/*-------- END DAILY SURVEY RESULTS CRUD -----------*/
+
+
+
+/*--------- BEGIN WEEKLY SURVEY RESULTS CRUD -----------*/
+
+//CREATE
+export async function createWeeklySurveyRecord(data) {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const length = 20;
+    let randomStr = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomNum = Math.floor(Math.random() * characters.length);
+        randomStr += characters[randomNum];
+    }
+    app.collection("weeklySurvey").doc(randomStr).set({answer:data.answer, questionID:data.questionID, surveyDateTime:data.surveyDateTime, surveyType:data.surveyType, uid:data.uid,})
+    console.log('Weekly Survey Record created');
+}
+
+//READ
+export async function getAllWeeklySurveyResults() { //returns all Weekly Survey Result records from the database
+    return app.collection("weeklySurvey").get();
+}
+
+export async function getWeeklySurveyResultByDocumentID(data) { //returns only the Weekly Survey Result document with the matching document ID
+    const record =  app.collection("weeklySurvey").doc(data.docID).get();
+    if (!record.exists) {
+        console.log('No document with that ID');
+        return;
+    } else {
+        return record.data;
+    }
+}
+
+export async function getWeeklySurveyResultByField(data) { //pass in the name of the field you want to search and the value to check for
+    const record = app.collection("weeklySurvey").where(data.fieldName, '==', data.fieldValue).get();
+    if (!record.exists) {
+        console.log('No document matches that value');
+        return;
+    } else {
+        return record.data;
+    }
+}
+
+//UPDATE
+export async function updateWeeklySurveyScore(data) { //Requires the Document ID to update the record
+    app.collection("weeklySurvey").doc(data.docID).update({ score:data.score });
+    console.log('Weekly Survey Score updated');
+}
+
+export async function updateWeeklySurveyDateTime(data) { //Requires the Document ID to update the record
+    app.collection("weeklySurvey").doc(data.docID).update({ testDateTime:data.testDateTime });
+    console.log('Weekly Survey DateTime updated');
+}
+
+//DELETE
+export async function deleteWeeklySurveyResultRecord(data) { //deletes entire record, it is possible to delete individual fields but not needed
+    app.collection("weeklySurvey").doc(data.docID).delete();
+    console.log('Weekly Survey Result Record deleted');
+}
+
+/*-------- END WEEKLY SURVEY RESULTS CRUD -----------*/
 
 
 export { firebase };
