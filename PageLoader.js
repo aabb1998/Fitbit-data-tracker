0dc3bf7b22bba124432e863bdeae80f59e63558c
muznau <48741723+muznau@users.noreply.github.com>
2021-10-25 08:22:10 +1100

    notification page loader

--- PageLoader.js
+++ PageLoader.js
@@ -5,3 +5,5 @@ export { default as HomeScreen } from "./src/Screens/HomeScreen/HomeScreen";
 export { default as RegistrationScreen } from "./src/Screens/RegistrationScreen/RegistrationScreen";
 
 export { default as SurveyScreen } from "./src/Screens/SurveyScreen/Survey";
+
+export { default as NotificationScreen } from "./src/Screens/NotificationScreen/NotificationScreen";
